c34b182109775bd5ecece2ff74f42bd1c0c4ad82
Malchemy <cmcd799@gmail.com>
2019-07-07 20:16:20 -0400

    Update encode.go

--- encode.go
+++ encode.go
@@ -35,7 +35,7 @@ var (
 
 // EncodeOptions is a set of options for encoding dca
 type EncodeOptions struct {
-	Volume           int              // change audio volume (256=normal)
+	Volume           int              // change audio volume (1=normal)
 	Channels         int              // audio channels
 	FrameRate        int              // audio sampling rate (ex 48000)
 	FrameDuration    int              // audio frame duration can be 20, 40, or 60 (ms)
@@ -92,7 +92,7 @@ func (opts *EncodeOptions) Validate() error {
 
 // StdEncodeOptions is the standard options for encoding
 var StdEncodeOptions = &EncodeOptions{
-	Volume:           25,
+	Volume:           0.2,
 	Channels:         2,
 	FrameRate:        48000,
 	FrameDuration:    20,
@@ -205,7 +205,7 @@ func (e *EncodeSession) run() {
 		"-f", "ogg",
 		"-vbr", vbrStr,
 		"-compression_level", strconv.Itoa(e.options.CompressionLevel),
-		"-vol", strconv.Itoa(e.options.Volume),
+		"-volume", strconv.Itoa(e.options.Volume),
 		"-ar", strconv.Itoa(e.options.FrameRate),
 		"-ac", strconv.Itoa(e.options.Channels),
 		"-b:a", strconv.Itoa(e.options.Bitrate * 1000),
